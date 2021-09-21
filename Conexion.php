7f55819a166d6eeb811bdd4998f6bdc2e8b9e0c6
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-09-20 20:02:40 -0500

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,8 +1,8 @@
 <?php 
-$host = $_ENV("FINDMYTAXI_HOST");  #Si es en otro lugar va el HTTP..
-$username = $_ENV("FINDMYTAXI_USER");
-$password = $_ENV("FINDMYTAXI_PASSWORD");
-$registro = $_ENV("FINDMYTAXI_REG");
+$host = $_ENV["FINDMYTAXI_HOST"];  #Si es en otro lugar va el HTTP..
+$username = $_ENV["FINDMYTAXI_USER"];
+$password = $_ENV["FINDMYTAXI_PASSWORD"];
+$registro = $_ENV["FINDMYTAXI_REG"];
 $connection = mysqli_connect($host, $username, $password, $registro); 
 
 ?>
