113f1f1da2e9acca8aaf851ded6083712beefb3e
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-09-20 20:41:55 -0500

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,8 +1,8 @@
 <?php 
-$host = getenv("FINDMYTAXI_HOST");  #Si es en otro lugar va el HTTP..
-$username = getenv("FINDMYTAXI_USER");
-$password = getenv("FINDMYTAXI_PASSWORD");
-$registro = getenv("FINDMYTAXI_REG");
+$host = "findmytaxi.cw0doizsxky1.us-east-2.rds.amazonaws.com";  #Si es en otro lugar va el HTTP..
+$username = "root";
+$password = "Uninorte2021";
+$registro = "findmytaxi";
 $connection = mysqli_connect($host, $username, $password, $registro); 
 
 ?>
