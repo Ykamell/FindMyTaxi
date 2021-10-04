55e63b184085c3f775532ae39c55904d8de5bcae
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-10-04 18:53:19 -0500

    Update consulta_historico.php

--- consulta_historico.php
+++ consulta_historico.php
@@ -1,5 +1,10 @@
 <?php
 include("Conexion.php"); 
-
+  if(isset($_POST['datetimes']) == 'Yes')
+  {
+    $startDate = strtotime($_POST['startDate']);
+    $endDate = strtotime($_POST['endDate']);
+    echo($startDate);
+  }
 }	
 ?>
