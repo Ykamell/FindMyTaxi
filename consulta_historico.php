3091c3cbe8e8d84ef63728a1ff473d1be70815d7
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-10-04 18:16:57 -0500

    Update consulta_historico.php

--- consulta_historico.php
+++ consulta_historico.php
@@ -1,13 +1,5 @@
 <?php
 include("Conexion.php"); 
-$startDate = strtotime($_POST['startDate']);
-$endDate = strtotime($_POST['endDate']);
-if(isset($startDate && $endDate) {
-	$historical = mysqli_query($connection,"SELECT * FROM location WHERE dateTime>=$startDate AND dateTime<=$endDate  ORDER BY ID ASC");
-	while($consulta=mysqli_fetch_array($historical)){
-		$poly[]=array((float) $consulta['lat'],(float) $consulta['lon']);
-	}
-	echo json_encode($poly);
-	echo $startDate;
+	$Date=$_POST["dateTimes"]
 }	
 ?>
