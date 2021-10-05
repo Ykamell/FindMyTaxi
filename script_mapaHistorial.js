e88b5763f8a4582ead1ad5ccecc8db2dd1f8cc7e
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-10-04 22:37:20 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -14,6 +14,7 @@ function enviarDatos(){
 		"data": frm
 	}).done(function(LatLon){
 		latlon = JSON.parse(LatLon);
+		console.log(latlon)
 		var inicial=new L.LatLng(latlon[0][0],latlon[0][1]);
 		myMarker.setLatLng(inicial).bindPopup('Ubicación inicial').openPopup();
 		var final=new L.LatLng(latlon[latlon.length -1][0],latlon[latlon.length -1][1]);
