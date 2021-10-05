a9cb4dffd66d22d7bb60f4ce28c64c13ccfb74b8
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-10-05 00:29:00 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -31,19 +31,22 @@ function enviarDatos(){
 }
 
 	function onMapClick(e) {
-		var latlon_click=e.latlng;
-		var min=5;
-		var Fecha=[];
-		var n=0;
-		for(var i = 0; i<= latlon.length-1; i++){
-			var distance = map.distance(latlon_click , [  latlon[i][0] , latlon[i][1]   ]);
-			if (distance < min) {
-				Fecha[n]=latlon[i][2];
-				n=n+1;
+		var checkBox = document.getElementById(“miCheckUbication”);
+		if (checkBox.checked == true){
+			var latlon_click=e.latlng;
+			var min=5;
+			var Fecha=[];
+			var n=0;
+			for(var i = 0; i<= latlon.length-1; i++){
+				var distance = map.distance(latlon_click , [  latlon[i][0] , latlon[i][1]   ]);
+				if (distance < min) {
+					Fecha[n]=latlon[i][2];
+					n=n+1;
+				}
 			}
+			popup
+			.setLatLng(e.latlng)
+			.setContent("Coordenadas geográficas: <br>" + latlon_click.toString()+'<br> Fechas: <br>' + Fecha.join('<br>') )
+			.openOn(map);
 		}
-		popup
-		.setLatLng(e.latlng)
-		.setContent("Coordenadas geográficas: <br>" + latlon_click.toString()+'<br> Fechas: <br>' + Fecha.join('<br>') )
-		.openOn(map);
 }
