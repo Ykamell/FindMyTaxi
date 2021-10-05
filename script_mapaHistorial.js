c4931fa846e67f63815025e400e94abd47bd5588
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-10-04 23:15:45 -0500

    Update script_mapaHistorial.js

--- script_mapaHistorial.js
+++ script_mapaHistorial.js
@@ -12,16 +12,8 @@ function enviarDatos(){
 		"method": "POST",
 		"url": "consulta_historico.php",
 		"data": frm,
-	}).done(function(LatLon){
-		latlon = JSON.parse(LatLon);
-		console.log(latlon);
-		var inicial=new L.LatLng(latlon[0][0],latlon[0][1]);
-		myMarker.setLatLng(inicial).bindPopup('Ubicación inicial').openPopup();
-		var final=new L.LatLng(latlon[latlon.length -1][0],latlon[latlon.length -1][1]);
-		 markery.setLatLng(final).bindPopup('Ubicación final').openPopup();
-		polyline.setLatLngs(latlon);
-		map.fitBounds(polyline.getBounds());	
-		map.on('click', onMapClick);		
+	}).done(function(LatLon1){
+		console.log("VALUE: ", LatLon1);		
 	});
 });
 
