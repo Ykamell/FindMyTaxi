f17ecaea2c9535542304e66658ac0f4b9a5d56fe
Ykamell <90073648+Ykamell@users.noreply.github.com>
2021-10-04 22:10:06 -0500

    Update consulta_historico.php

--- consulta_historico.php
+++ consulta_historico.php
@@ -1,20 +1,22 @@
 <?php
 include("Conexion.php"); 
-$startDate = strval($_POST['startDate']);
-$endDate = strval($_POST['endDate']);
-$date_i = intval(preg_replace('/[^0-9]+/', '', $startDate), 10); 
-$date_f = intval(preg_replace('/[^0-9]+/', '', $endDate), 10);
-echo $startDate;
-echo " ";
-echo $endDate;
-echo " ";
-echo $date_i;
-echo " ";
-echo $date_f;
+if(isset($_POST["starDate"]) && $_POST["endDate"]) {
+	$startDate = strval($_POST['startDate']);
+	$endDate = strval($_POST['endDate']);
+	$date_i = intval(preg_replace('/[^0-9]+/', '', $startDate), 10); 
+	$date_f = intval(preg_replace('/[^0-9]+/', '', $endDate), 10);
+	echo $startDate;
+	echo " ";
+	echo $endDate;
+	echo " ";
+	echo $date_i;
+	echo " ";
+	echo $date_f;
 
-$historical = mysqli_query($connection,"SELECT * FROM location WHERE dateTime>=$date_i AND dateTime<=$date_f ORDER BY ID ASC");
-while($consulta=mysqli_fetch_array($historical)){
-	$poly[]=array((float) $consulta['lat'],(float) $consulta['lon'],(string) $consulta['dateTime']);		
+	$historical = mysqli_query($connection,"SELECT * FROM location WHERE dateTime>=$date_i AND dateTime<=$date_f ORDER BY ID ASC");
+	while($consulta=mysqli_fetch_array($historical)){
+		$poly[]=array((float) $consulta['lat'],(float) $consulta['lon'],(string) $consulta['dateTime']);		
+	}
+	echo json_encode($poly);
 }
-echo json_encode($poly);
 ?>
